5198e583d06b63780740d7a31661140abc94d163
userlog404 <classyneon@gmail.com>
2024-03-05 07:18:10 +0000

    final push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import {useState} from 'react';
 
-function App() {
+/* const Person = (props) => {
+  return (
+    <>
+      <h1>Name: {props.name}</h1>
+      <h2>Age: {props.age}</h2>
+      <h3>Gender: N/A</h3>
+    </>
+  );
+}
+ */
+/* const App = () => {
+  const there = 8;
+  const isThere = false;
+  const big = "I am a big guy!"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <h1>0001 React Course</h1>
+      <p>Hello there, this is a dynamic react app constituting to: {there * 8 / 5} bucks! </p>
+      <p>Printing there: {isThere ? there : "there is 404"}</p>
+
+      {big ? (
+        <>
+          <h2>{big}</h2>
+          <Person
+            age={89 - 9}
+            name="Echo"
+          />
+          <Person
+            name="chin"
+            age={27}
+
+          />
+          <Person />
+          <Person />
+          <Person />
+
+
+        </>
+
+      ) : (
+        <h3>this is a h3</h3>
+
+      )
+      }
+    </div> */
+
+/*   );
+} */
+
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    alert(" Counter changed to: " + counter)
+  }, [counter]);
+  
+  return (
+      <>
+        <div className="App">
+          <button onClick={(prevCounter) => setCounter(prevCounter => prevCounter + 1)}>+</button>
+          <h2>{counter}</h2>
+          <button onClick={(prevCounter) => setCounter(prevCounter => prevCounter - 1)}>-</button>
+        </div>
+      </>
+/*       <div className="App">
+        <button onClick={() => setCounter(counter+1)}>+</button>
+        <h2>{counter}</h2>
+        <button onClick={() => setCounter(counter-1)}>-</button>
+      </div>*/
+    ); 
 }
 
 export default App;
